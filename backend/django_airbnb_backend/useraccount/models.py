f8a95b5a5a0dd0f48b2a94ec5c04b2f3aa9cf3c2
Junseok Hur <gjwnstjr100@gmail.com>
2024-08-07 06:51:58 -0400

    create app, model, and serializer for properties

--- backend/django_airbnb_backend/useraccount/models.py
+++ backend/django_airbnb_backend/useraccount/models.py
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255, blank=True, null=True)
     avatar = models.ImageField(upload_to='uploads/avatars')
 
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
